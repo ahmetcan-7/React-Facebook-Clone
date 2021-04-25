11369ba6a94ac9609771bdb0c8ddc2e26abaa7bd
Ahmet CAN <ahmetcan423507@gmail.com>
2021-04-25 15:25:03 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
@@ -6,29 +6,65 @@ import Feed from "./components/Feed"
 import Widgets from "./components/Widgets";
 import Login from "./components/Login";
 import { useStateValue } from './contexts/StateProvider';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
 
-
+  const carRef = React.createRef();
   return (
-    <div className="app">
-      {
-        !user ? <Login />
-          : (
-            <>
-              <Header />
+    <Router >
+      <div className="app">
+        {
 
-              <div className="app__body">
-                <SideBar />
-                <Feed />
-                <Widgets />
-              </div>
-            </>
-          )
-      }
+          !user ? <Login />
+            : (
 
-    </div>
+              <>
+                <Switch>
+
+                  <Route path="/sidebar">
+
+                    <Header carRef={carRef} />
+
+                    <div className="app__body">
+                      <SideBar carRef={carRef} />
+                    </div>
+
+
+                  </Route>
+
+                  <Route path="/">
+
+
+                    <Header carRef={carRef} />
+
+                    <div className="app__body">
+                      <SideBar carRef={carRef} />
+                      <Feed />
+                      <Widgets />
+                    </div>
+
+
+                  </Route>
+
+
+
+
+
+
+
+
+                </Switch>
+              </>
+
+            )
+
+        }
+
+      </div>
+    </Router>
+
   );
 }
 
